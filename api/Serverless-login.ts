9c37c81844a6d04183d0078272f3d0e72ae3dd75
vale-sch <vali.vogt@live.de>
2023-11-05 19:32:40 +0100

    start server

--- api/Serverless-login.ts
+++ api/Serverless-login.ts
@@ -42,3 +42,28 @@ export default corsHandler(async (req: VercelRequest, res: VercelResponse) => {
         await client.close();
     }
 });
+//@ts-ignore
+const allowCors = fn => async (req: any, res: any) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = (req: any, res: any) => {
+    const d = new Date()
+    res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+
--- src/Serverless-login.js
+++ src/Serverless-login.js
@@ -47,3 +47,22 @@ exports.default = corsHandler((req, res) => __awaiter(void 0, void 0, void 0, fu
         yield client.close();
     }
 }));
+//@ts-ignore
+const allowCors = fn => (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return yield fn(req, res);
+});
+const handler = (req, res) => {
+    const d = new Date();
+    res.end(d.toString());
+};
+module.exports = allowCors(handler);
