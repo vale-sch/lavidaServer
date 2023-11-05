1badc8697956b32bd3f079c2e6bbcdbf00636366
vale-sch <vali.vogt@live.de>
2023-11-05 18:01:08 +0100

    start server

--- src/Serverless-login.js
+++ src/Serverless-login.js
@@ -9,11 +9,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('micro-cors')({
-    origin: ['https://vale-sch.github.io', 'http://127.0.0.1:5500'], // Replace with your origins
+const cors = require('cors')({
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
 });
 const uri = 'mongodb+srv://LaVidaAdmin:password123123@lavida.pdmcc5b.mongodb.net/?retryWrites=true&w=majority';
 console.error("establishe-1");
+(req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    console.error("established0");
+    // Use CORS middleware to handle CORS headers
+    cors(req, res);
+    console.error("established11");
+    try {
+        yield client.connect();
+        console.error("connection established");
+        // Handle your login logic here using req and res
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    finally {
+        yield client.close();
+    }
+});
 module.exports = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const client = new MongoClient(uri, {
         serverApi: {
