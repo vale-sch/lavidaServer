4b01a198afb7e99b352a5129fb3b4fc632937274
vale-sch <vali.vogt@live.de>
2023-11-05 17:20:07 +0100

    start server

--- api/Serverless-login.ts
+++ api/Serverless-login.ts
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')({
-    origin: ['https://vale-sch.github.io', 'http://127.0.0.1:5500'],
+    origin: ['https://vale-sch.github.io', 'http://127.0.0.1:5500', "https://lavida-server-fijmyalhx-vale-schs-projects.vercel.app/"]
 });
 
 const uri = 'mongodb+srv://LaVidaAdmin:password123123@lavida.pdmcc5b.mongodb.net/?retryWrites=true&w=majority';
