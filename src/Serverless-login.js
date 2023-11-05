fe30ce71a3e7618c41ef76270c9135436a347611
vale-sch <vali.vogt@live.de>
2023-11-05 18:12:52 +0100

    start server

--- src/Serverless-login.js
+++ src/Serverless-login.js
@@ -16,31 +16,6 @@ const cors = require('cors')({
 });
 const uri = 'mongodb+srv://LaVidaAdmin:password123123@lavida.pdmcc5b.mongodb.net/?retryWrites=true&w=majority';
 console.error("establishe-1");
-(req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-    console.error("established0");
-    // Use CORS middleware to handle CORS headers
-    cors(req, res);
-    console.error("established11");
-    try {
-        yield client.connect();
-        console.error("connection established");
-        // Handle your login logic here using req and res
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-    finally {
-        yield client.close();
-    }
-});
 module.exports = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const client = new MongoClient(uri, {
         serverApi: {
