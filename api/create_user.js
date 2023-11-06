f04b73215547a2ea80af9cc451180dfe77f2f2bc
vale-sch <vali.vogt@live.de>
2023-11-06 16:10:08 +0100

    added postgres vercel

--- api/create_user.js
+++ api/create_user.js
@@ -9,17 +9,8 @@ const pool = new Pool({
 });
 
 export default async (req, res) => {
-  if (req.method === 'OPTIONS') {
-    // Set the necessary CORS headers to allow requests from any origin
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    res.status(204).end(); // Respond with a 204 No Content status for preflight
-  } else if (req.method === 'POST') {// Ensure the server function only responds to POST requests
+  if (req.method === 'POST') {
     const { id, name, password } = req.body;
-    console.log(req.body);
 
     if (!id || !name || !password) {
       return res.status(400).json({ error: 'ID, name, and password are required' });
@@ -27,7 +18,7 @@ export default async (req, res) => {
 
     try {
       const result = await pool.query(
-        'INSERT INTO users (Id, Name, Password) VALUES ($1, $2, $3)',
+        'INSERT INTO users (id, name, password) VALUES ($1, $2, $3)',
         [id, name, password]
       );
       res.status(201).json({ message: 'User created successfully' });
