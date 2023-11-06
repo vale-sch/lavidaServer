970581389d1f4a83c8132ba1dfe117118847ef85
vale-sch <vali.vogt@live.de>
2023-11-06 14:52:27 +0100

    added postgres vercel

---
+++ api/create_user.js
@@ -0,0 +1,31 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const { name, password } = req.body;
+
+    if (!name || !password) {
+      return res.status(400).json({ error: 'Name and password are required' });
+    }
+
+    try {
+      const result = await pool.query(
+        'INSERT INTO users (name, password) VALUES ($1, $2) RETURNING id',
+        [name, password]
+      );
+      res.status(201).json({ id: result.rows[0].id });
+    } catch (error) {
+      console.error('Error executing the query:', error);
+      res.status(500).json({ error: 'An error occurred while creating the user' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+};
---
+++ api/get_users.js
@@ -0,0 +1,23 @@
+// api/getAllUsers.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const result = await pool.query('SELECT * FROM users');
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error('Error executing the query:', error);
+      res.status(500).json({ error: 'An error occurred while fetching users' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+};
