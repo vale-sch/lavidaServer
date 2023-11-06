b01e84375bd42cce153e37789e0f014dde7b1e85
vale-sch <vali.vogt@live.de>
2023-11-06 16:09:14 +0100

    added postgres vercel

--- api/create_user.js
+++ api/create_user.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false,
   }
