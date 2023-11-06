d5f6cc1c581259ed15bae30ac63128b39931fc8e
vale-sch <vali.vogt@live.de>
2023-11-06 15:11:05 +0100

    added postgres vercel

--- api/create_user.js
+++ api/create_user.js
@@ -1,26 +1,27 @@
-import { Pool } from ('pg');
+// api/createUser.js
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
-  },
+  }
 });
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   if (req.method === 'POST') {
-    const { name, password } = req.body;
+    const { id, name, password } = req.body;
 
-    if (!name || !password) {
-      return res.status(400).json({ error: 'Name and password are required' });
+    if (!id || !name || !password) {
+      return res.status(400).json({ error: 'ID, name, and password are required' });
     }
 
     try {
       const result = await pool.query(
-        'INSERT INTO users (name, password) VALUES ($1, $2) RETURNING id',
-        [name, password]
+        'INSERT INTO users (id, name, password) VALUES ($1, $2, $3)',
+        [id, name, password]
       );
-      res.status(201).json({ id: result.rows[0].id });
+      res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
       console.error('Error executing the query:', error);
       res.status(500).json({ error: 'An error occurred while creating the user' });
--- api/get_users.js
+++ api/get_users.js
@@ -1,5 +1,5 @@
 // api/getAllUsers.js
-import { Pool } from ('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
