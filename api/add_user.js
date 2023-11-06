05c0da14ed4cb8a53d0e9944a4b763e808c8af44
vale-sch <vali.vogt@live.de>
2023-11-06 14:19:53 +0100

    added postgres vercel

--- api/add_user.js
+++ api/add_user.js
@@ -2,11 +2,12 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(request, response) {
   try {
+    const id = request.query.id;
     const name = request.query.name;
     const password = request.query.password;
 
     if (!password || !name) throw new Error('Name and Password is required.');
-    await sql`INSERT INTO users (Name, Password) VALUES (${password}, ${name});`;
+    await sql`INSERT INTO users (Id ,Name, Password) VALUES (${id},${password}, ${name});`;
   } catch (error) {
     console.log(error);
     return response.status(500).json({ error });
