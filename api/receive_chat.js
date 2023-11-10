6a88b9691253c838a66472f028405d50e2204636
vale-sch <vali.vogt@live.de>
2023-11-10 16:43:51 +0100

    further progress in realtime db

--- api/receive_chat.js
+++ api/receive_chat.js
@@ -2,7 +2,15 @@ import supabase from "../utils/supabase";
 
 // Assuming you have a route or endpoint to handle fetching messages by chat ID
 module.exports = async (req, res) => {
-  if (req.method === "GET") {
+  if (req.method === "OPTIONS") {
+    // Set the necessary CORS headers to allow requests from the specific origin without a trailing slash
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    res.status(204).end(); // Respond with a 204 No Content status for preflight
+  } else if (req.method === "POST") {
     try {
       const { chatID } = req.query; // Assuming chatID is passed as a query parameter
 
