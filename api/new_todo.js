1a6d12bf21732bd61caf4003ed75847e84c7cbe6
vale-sch <vali.vogt@live.de>
2023-11-10 10:54:28 +0100

    added supabase

--- api/new_todo.js
+++ api/new_todo.js
@@ -3,6 +3,7 @@ module.exports = async (req, res) => {
   if (req.method === "GET") {
     try {
       await supabase.from("todos").insert({ kevin: "jo" });
+      res.status(201)("success!");
     } catch (error) {
       console.error("Error executing the query:", error);
       res.status(500).json({ error: "An error occurred while fetching users" });
