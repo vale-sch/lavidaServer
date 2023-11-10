c19a3308d636d4189bf956e423a0429aa8ce82a7
vale-sch <vali.vogt@live.de>
2023-11-10 10:45:43 +0100

    added supabase

---
+++ api/new_todo.js
@@ -0,0 +1,16 @@
+import supabase from "../utils/supabase";
+module.exports = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      e.preventDefault();
+      await supabase.from("todos").insert({ title });
+      reload();
+      setTitle("");
+    } catch (error) {
+      console.error("Error executing the query:", error);
+      res.status(500).json({ error: "An error occurred while fetching users" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+};
