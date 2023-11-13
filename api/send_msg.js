db1f6522a58b27129ab1eb260d6fa4c4eaaa8aaa
vale-sch <vali.vogt@live.de>
2023-11-13 16:03:06 +0100

    added more function

--- api/send_msg.js
+++ api/send_msg.js
@@ -2,76 +2,56 @@ import supabase from "../utils/supabase";
 import ChatHistory from "../utils/chat_history.js";
 
 module.exports = async (req, res) => {
+  const allowedOrigins = ["http://127.0.0.1:5500"];
+
   if (req.method === "OPTIONS") {
-    // Set the necessary CORS headers to allow requests from the specific origin without a trailing slash
-    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    // Set CORS headers for preflight requests
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigins.join(","));
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    res.status(204).end(); // Respond with a 204 No Content status for preflight
+    res.status(204).end();
   } else if (req.method === "POST") {
-    let body = req.body.chatID;
     try {
-      let chatHistory = new ChatHistory(
-        req.body.chat_id,
-        req.body.messages || []
-      );
-      // Get the current chat entry
+      const { chat_id, messages = [] } = req.body;
+
+      // Fetch existing chat
       const { data: existingChat, error: existingChatError } = await supabase
         .from("chat_history")
         .select()
-        .eq("chat_id", chatHistory.chat_id);
+        .eq("chat_id", chat_id);
 
       if (existingChatError) {
-        console.error("Error fetching existing chat:", existingChatError);
-        res.status(500).json({
-          error: "An error occurred while fetching the existing chat",
-          body,
-        });
-        return;
+        throw existingChatError;
       }
 
-      // If the chat doesn't exist, create a new one
-      if (existingChat.length === 0) {
-        const { data: newChat, error: newChatError } = await supabase
-          .from("chat_history")
-          .insert([chatHistory.toDatabase()]);
+      const chatHistory = new ChatHistory(chat_id, messages);
 
-        if (newChatError) {
-          console.error("Error creating new chat:", newChatError);
-          res.status(500).json({
-            error: "An error occurred while creating the new chat",
-            body,
-          });
-        } else {
-          res.status(201).json(chatHistory);
-        }
-      } else {
-        // If the chat exists, update the messages array
-        const updatedMessages = [
-          ...existingChat[0].messages,
-          ...chatHistory.messages,
-        ];
+      // Create or update chat
+      const { data: updatedChat, error: updateChatError } =
+        existingChat.length === 0
+          ? await supabase
+              .from("chat_history")
+              .insert([chatHistory.toDatabase()])
+          : await supabase
+              .from("chat_history")
+              .update({
+                messages: [
+                  ...existingChat[0].messages,
+                  ...chatHistory.messages,
+                ],
+              })
+              .eq("chat_id", chat_id);
 
-        const { data: updatedChat, error: updateChatError } = await supabase
-          .from("chat_history")
-          .update({ messages: updatedMessages })
-          .eq("chat_id", chatHistory.chat_id);
-
-        if (updateChatError) {
-          console.error("Error updating chat messages:", updateChatError);
-          res.status(500).json({
-            error: "An error occurred while updating the chat messages",
-            body,
-          });
-        } else {
-          res.status(201).json(chatHistory);
-        }
+      if (updateChatError) {
+        throw updateChatError;
       }
+
+      res.status(201).json(chatHistory);
     } catch (error) {
       console.error("Error processing the request:", error);
-      res.status(500).json({ error: "An unexpected error occurred", body });
+      res.status(500).json({ error: "An unexpected error occurred" });
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
