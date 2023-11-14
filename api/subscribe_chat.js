e97679850f670c91c8fe83ad4ee39b126c9213fa
vale-sch <vali.vogt@live.de>
2023-11-14 15:09:22 +0100

    added subscribtion

--- api/subscribe_chat.js
+++ api/subscribe_chat.js
@@ -1,5 +1,27 @@
-import websocket from "../utils/websocket";
+import { createServer } from "http";
+import { Server } from "ws";
 import supabase from "../utils/supabase";
+
+const server = createServer();
+const wss = new Server({ noServer: true });
+
+wss.on("connection", (ws) => {
+  // Handle WebSocket connection
+
+  ws.on("message", (message) => {
+    // Handle incoming messages
+    console.log(`Received message: ${message}`);
+  });
+
+  // Send a welcome message
+  ws.send("Welcome to the WebSocket server!");
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
 
 export default async (req, res) => {
   if (req.method === "OPTIONS") {
@@ -13,6 +35,9 @@ export default async (req, res) => {
     try {
       // Get the chatID from the query parameters
       const { chatID } = req.query;
+      const websocket = new WebSocket(
+        process.env.NEXT_PUBLIC_SUPABASE_URL.replace("http", "ws")
+      );
 
       // Handle WebSocket events
       websocket.addEventListener("open", () => {
@@ -21,7 +46,7 @@ export default async (req, res) => {
           .from("chat_history")
           .on("INSERT", (payload) => {
             // Send the new chat data to the connected clients
-            socket.send(JSON.stringify(payload));
+            websocket.send(JSON.stringify(payload));
           })
           .eq("chat_id", chatID)
           .subscribe();
@@ -34,7 +59,7 @@ export default async (req, res) => {
       });
 
       // Return the array of messages
-      res.status(200).text("Subscription was successfull!");
+      res.status(200).text("Subscription was successful!");
     } catch (error) {
       console.error("Error processing the request:", error);
       res.status(500).json({ error: "An unexpected error occurred" });
--- utils/websocket.js
+++
@@ -1,6 +0,0 @@
-import { WebSocket } from "ws";
-
-export default WebSocket(
-  // Establish a WebSocket connection with Supabase
-  new WebSocket(process.env.NEXT_PUBLIC_SUPABASE_URL.replace("http", "ws"))
-);
