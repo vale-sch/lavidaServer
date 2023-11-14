dc4e19d04a814c3466b6e6fd1624f9a41dc66666
vale-sch <vali.vogt@live.de>
2023-11-14 15:22:38 +0100

    added subscribtion

--- api/subscribe_chat.js
+++ api/subscribe_chat.js
@@ -2,12 +2,13 @@ import { Server } from "http";
 import { WebSocketServer } from "ws";
 import supabase from "../utils/supabase";
 
+// Create an HTTP server
 const server = new Server();
-const wss = new WebSocketServer({ server });
 
+// Create a WebSocket server and attach it to the HTTP server
+const wss = new WebSocketServer({ noServer: true });
 wss.on("connection", (ws) => {
   // Handle WebSocket connection
-
   ws.on("message", (message) => {
     // Handle incoming messages
     console.log(`Received message: ${message}`);
@@ -15,6 +16,19 @@ wss.on("connection", (ws) => {
 
   // Send a welcome message
   ws.send("Welcome to the WebSocket server!");
+});
+
+// Attach the WebSocket server to the HTTP server
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
+
+// Start the HTTP server on a specified port
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default async (req, res) => {
@@ -30,27 +44,12 @@ export default async (req, res) => {
       // Get the chatID from the query parameters
       const { chatID } = req.query;
 
-      // Handle WebSocket events
-      wss.on("connection", (ws) => {
-        // Subscribe to the chat changes for the specified chatID
-        const subscription = supabase
-          .from("chat_history")
-          .on("INSERT", (payload) => {
-            // Send the new chat data to the connected clients
-            ws.send(JSON.stringify(payload));
-          })
-          .eq("chat_id", chatID)
-          .subscribe();
-
-        ws.on("close", () => {
-          // Clean up resources on socket close
-          // For example, close the Supabase subscription
-          subscription.close();
-        });
-      });
-
       // Return the array of messages
-      res.status(201).send(`Subscription was successful! Listen on ${ws}`);
+      res
+        .status(201)
+        .send(
+          `Subscription was successful! Listen on wss://lavida-server.vercel.app/ws?chatID=${chatID}`
+        );
     } catch (error) {
       console.error("Error processing the request:", error);
       res.status(500).json({ error: "An unexpected error occurred" });
