926225ae619751edb22b4fab15929de6b5767e39
vale-sch <vali.vogt@live.de>
2023-11-14 15:01:31 +0100

    added subscribtion

--- utils/websocket.js
+++ utils/websocket.js
@@ -1,6 +1,6 @@
 import { WebSocket } from "ws";
 
-export default createWebSocketStream(
+export default WebSocket(
   // Establish a WebSocket connection with Supabase
   new WebSocket(process.env.NEXT_PUBLIC_SUPABASE_URL.replace("http", "ws"))
 );
