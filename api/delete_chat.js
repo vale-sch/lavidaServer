7be372484a5e1f385bfaad2db014220751f8f89c
vale-sch <vali.vogt@live.de>
2023-12-12 19:50:31 +0100

    added github in cors

--- api/delete_chat.js
+++ api/delete_chat.js
@@ -4,11 +4,8 @@ import supabase from "../utils/supabase";
 module.exports = async (req, res) => {
   if (req.method === "OPTIONS") {
     // Set CORS headers for preflight requests
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "http://127.0.0.1:5500",
-      "https://vale-sch.github.io"
-    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", "true");
--- api/push_chat_to_user.js
+++ api/push_chat_to_user.js
@@ -13,11 +13,8 @@ export default async (req, res) => {
   // Handle preflight OPTIONS request for CORS
   if (req.method === "OPTIONS") {
     // Set the necessary CORS headers to allow requests from the specific origin without a trailing slash
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "http://127.0.0.1:5500",
-      "https://vale-sch.github.io"
-    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
     res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", "true");
