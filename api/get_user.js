8bbee988ea4cb0e509e9480c41f5b218b698007a
vale-sch <vali.vogt@live.de>
2023-12-21 15:08:30 +0100

    get_user api endpoint addded

---
+++ api/get_user.js
@@ -0,0 +1,45 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default async (req, res) => {
+  // Handle preflight OPTIONS request for CORS
+  if (req.method === "OPTIONS") {
+    // Set the necessary CORS headers to allow requests from the specific origin without a trailing slash
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    res.status(204).end(); // Respond with a 204 No Content status for preflight
+  } else if (req.method === "POST") {
+    const { name } = req.query; // Assuming the query parameter is 'name'
+
+    if (!name) {
+      return res.status(400).json({ error: "Name parameter is required" });
+    }
+
+    try {
+      const result = await pool.query("SELECT * FROM users WHERE name = $1", [
+        name,
+      ]);
+
+      if (result.rows.length > 0) {
+        res.status(200).json(result.rows[0]); // Sending the first matching user found
+      } else {
+        res.status(404).json({ error: "User not found" });
+      }
+    } catch (error) {
+      console.error("Error executing the query:", error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while retrieving the user" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+};
